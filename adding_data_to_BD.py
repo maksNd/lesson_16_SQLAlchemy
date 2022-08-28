14d0fdd91f77282c8a6caca1b95ed9802653507c
mkNd <mknord1565@gmail.com>
2022-08-28 15:13:39 +0300

    improvements

--- adding_data_to_BD.py
+++ adding_data_to_BD.py
@@ -9,6 +9,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
@@ -34,6 +35,8 @@ class User(db.Model):
     name = Column(String(100), nullable=False)  # это поле должно быть заполнено
     age = Column(Integer, CheckConstraint('age > 18'))  # ограничение по условию
     group_id = Column(Integer, ForeignKey('group.id'))
+    """Строчка ниже делает тоже самое что и строчка выше, только важен порядок создания классов таблиц"""
+    # group_id = Column(Integer, ForeignKey(Group.id))
 
     group = relationship('Group')
 
@@ -61,8 +64,9 @@ user_05 = User(id=5, name='Lily', age=19, group=group_02)
 user_06 = User(id=6, name='Mary', age=26, group=group_02)
 users = [user_06, user_05, user_04, user_03, user_02, user_01]
 
-with db.session.begin():  # мы не добаляем группы, т.к. адхимия добавит их автоматически
+with db.session.begin():  # мы не добаляем группы, т.к. алхимия добавит их автоматически с помощью relationship
     db.session.add_all(users)
+    # db.session.add_all([group_02, group_01])
 
 # print(User.query.all())
 """
